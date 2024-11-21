682ae6424eb6e0930412f07270db745e7b25729c
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-11-21 16:18:06 -0500

    finished e2e and component testing

--- client/cypress/component/Quiz.cy.tsx
+++ client/cypress/component/Quiz.cy.tsx
@@ -1,7 +1,13 @@
-describe('Site Tests', () => {
-  it('passes', () => {
-    cy.visit('http://127.0.1:3000');
+import React from 'react'
+import { mount } from 'cypress/react18'
+import Quiz from '../../src/components/Quiz'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+describe('<Quiz />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    mount(<Quiz />)
     cy.get('button').contains('Start Quiz').click();
     cy.get('button').contains('1').click();
     cy.get('button').contains('2').click();
@@ -15,4 +21,4 @@ describe('Site Tests', () => {
     cy.get('button').contains('2').click();
     cy.get('button').contains('Take New Quiz').click();
   })
-});
+})
